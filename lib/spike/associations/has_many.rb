9bee9265a25cdb57b52b33635dca7e7aa10fdc0e
Jens Balvig <jens@balvig.com>
2014-11-05 18:36:09 +0900

    clean up merge code a bit

--- lib/spike/associations/has_many.rb
+++ lib/spike/associations/has_many.rb
@@ -16,14 +16,11 @@ module Spike
       end
 
       def assign_nested_attributes(collection)
-        #TODO: Clean this up!!1
         collection = collection.values if collection.is_a?(Hash)
 
         collection.each do |attributes|
-          existing = Array(parent.attributes[name]).index { |r| r[:id] && r[:id].to_s == attributes.with_indifferent_access[:id].to_s }
-          if existing
-            existing_attributes = parent.attributes[name][existing]
-            parent.attributes[name][existing] = existing_attributes.merge(attributes)
+          if existing = find_existing_attributes(attributes['id'])
+            existing.merge!(attributes)
           else
             build(attributes)
           end
@@ -31,6 +28,10 @@ module Spike
       end
 
       private
+
+        def find_existing_attributes(id)
+          parent.attributes[name].to_a.find { |attr| attr[:id] && attr[:id].to_s == id.to_s }
+        end
 
         def add_to_parent(record)
           parent.attributes[name] ||= []
