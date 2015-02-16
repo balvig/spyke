ec7bec197aceb341faaedaa32fb42cc06924508e
Jens Balvig <jens@balvig.com>
2015-02-16 16:57:48 +0100

    log "missed" request when testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,3 +16,10 @@ Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
 # Pretty colors
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+# Don't raise but report uncaught net connections
+WebMock.allow_net_connect!
+WebMock.stub_request(:any, /.*/).to_return do |request|
+  puts "\e[35mUNSTUBBED REQUEST:\e[0m #{request.method.upcase} #{request.uri}"
+  { body: nil }
+end
