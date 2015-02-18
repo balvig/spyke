9788715a986efc348e4ca8eb3b190b9c3538cfe7
Jens Balvig <jens@balvig.com>
2015-02-18 19:18:12 +0100

    move webmock config to support file

--- test/support/webmock.rb
+++ test/support/webmock.rb
@@ -1,6 +1,15 @@
+require 'webmock/minitest'
+
 class WebMock::RequestStub
   def to_return_json(hash, options = {})
     options[:body] = MultiJson.dump(hash)
     to_return(options)
   end
+end
+
+# Don't raise but report uncaught net connections
+WebMock.allow_net_connect!
+WebMock.stub_request(:any, /.*/).to_return do |request|
+  puts "\e[35mUNSTUBBED REQUEST:\e[0m #{request.method.upcase} #{request.uri}"
+  { body: nil }
 end
