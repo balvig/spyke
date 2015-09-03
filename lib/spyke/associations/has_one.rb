4648ed1f37a0f80ba85fe52f53c5bcae86b26f4a
Jens Balvig <jens@balvig.com>
2015-09-03 11:15:19 +0200

    Fix foreign keys being added to embed-only objects
    Fixes #58
    Embedded associations that have no URI do not need to have the foreign
    key added when built through a parent.

--- lib/spyke/associations/has_one.rb
+++ lib/spyke/associations/has_one.rb
@@ -4,7 +4,7 @@ module Spyke
       def initialize(*args)
         super
         @options.reverse_merge!(uri: "#{parent.class.model_name.plural}/:#{foreign_key}/#{@name}")
-        @params[foreign_key] = parent.id
+        @params[foreign_key] = parent.id unless embed_only?
       end
     end
   end
