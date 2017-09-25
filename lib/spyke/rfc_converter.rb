1db9958ce4f346bee0854bf32d0d42c11f592c8f
Jens Balvig <jens@balvig.com>
2017-09-25 17:20:24 +0900

    lose one method

--- lib/spyke/rfc_converter.rb
+++ lib/spyke/rfc_converter.rb
@@ -7,8 +7,7 @@ module Spyke
     def convert
       output = @input.dup
       output = wrap_required_variables_in_curly_braces(output)
-      output = convert_start_parens_curly_braces(output)
-      output = convert_end_parens_to_curly_braces(output)
+      output = convert_parens_to_curly_braces(output)
       output = remove_colons(output)
       output
     end
@@ -18,12 +17,8 @@ module Spyke
         text.gsub(/(:\w+(?!\)))\b/, '{\1}')
       end
 
-      def convert_start_parens_curly_braces(text)
-        text.gsub('(', '{')
-      end
-
-      def convert_end_parens_to_curly_braces(text)
-        text.gsub(')', '}')
+      def convert_parens_to_curly_braces(text)
+        text.gsub('(', '{').gsub(')', '}')
       end
 
       def remove_colons(text)
