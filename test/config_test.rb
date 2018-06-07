cff486a4d005983ce08f83436e8be589613ad74a
Johan Smits <johan.smits@leftclick.eu>
2018-06-07 09:45:25 +0200

    Add test coverage for the deprecated connection setup.
    Ensure it raises the deprecation warning.

---
+++ test/config_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+module Spyke
+  class ConfigConnectionWarnTest < MiniTest::Test
+
+    def test_config_connection_warn
+      assert_output '', "[DEPRECATION] `Spyke::Config.connection=` is deprecated.  Please use `Spyke::Base.connection=` instead.\n" do
+        Spyke::Config.connection = Spyke::Base.connection
+      end
+    end
+  end
+end
