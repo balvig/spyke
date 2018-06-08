d727997cd4727b17d1bb5903d8b621f5d8d40dcc
Jens Balvig <jens@balvig.com>
2018-06-08 15:26:01 +0900

    remove extra linebreak

--- test/config_test.rb
+++ test/config_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 module Spyke
   class ConfigConnectionWarnTest < MiniTest::Test
-
     def test_config_connection_warn
       assert_output '', "[DEPRECATION] `Spyke::Config.connection=` is deprecated.  Please use `Spyke::Base.connection=` instead.\n" do
         Spyke::Config.connection = Spyke::Base.connection
