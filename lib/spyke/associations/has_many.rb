40329a0c7d2b0667a00256a13c5bb2c24da48858
Xavier Bick <fxb9500@gmail.com>
2019-12-06 12:40:00 -0600

    Make sure if the incoming nested attributes for a has_many association are an ActionController:Params instance, we convert to hash first.  Fixes #111

--- lib/spyke/associations/has_many.rb
+++ lib/spyke/associations/has_many.rb
@@ -12,6 +12,13 @@ module Spyke
       end
 
       def assign_nested_attributes(incoming)
+        # This ensures that if we are in a verison of Rails where params are
+        # an instance of ActionController::Params instad of Hash, we convert
+        # the params to a Hash.  Note that if the ActionController::Params
+        # instance is not permitted (strong params), an error is raised on .to_h
+        if incoming.respond_to?(:permitted?)
+          incoming = incoming.to_h
+        end
         incoming = incoming.values if incoming.is_a?(Hash)
         combined_attributes = combine_with_existing(incoming)
         clear_existing!
