794e0c174c277e54d2d9a302e00936195c9e6358
Nick Campbell <nc@zaikio.com>
2022-01-05 14:40:45 +0000

    Replace ActiveSupport::PerThreadRegistry with Thread.current variable
    This resolves a warning from Rails 7:
    
      DEPRECATION WARNING: ActiveSupport::PerThreadRegistry is deprecated and will be removed in Rails 7.1.
      Use `Module#thread_mattr_accessor` instead.
        (called from extend at /home/circleci/project/lib/spyke/scope_registry.rb:3
    
    Unfortunately, `thread_mattr_accessor` was only introduced in Rails 5,
    and the behaviour significantly changed in Rails 6, so it isn't a stable
    replacement API. Instead, we can just use a thread-local variable, which
    is how Rails itself does it:
    https://github.com/rails/rails/blob/6a8c51b19b6a20511b26d486419ccaebbca93419/activesupport/lib/active_support/core_ext/module/attribute_accessors_per_thread.rb

--- lib/spyke/scope_registry.rb
+++ lib/spyke/scope_registry.rb
@@ -1,17 +1,19 @@
 module Spyke
-  class ScopeRegistry
-    extend ActiveSupport::PerThreadRegistry
-
-    def initialize
-      @registry = Hash.new { |hash, key| hash[key] = {} }
-    end
+  module ScopeRegistry
+    extend self
 
     def value_for(scope_type, variable_name)
-      @registry[scope_type][variable_name]
+      registry[scope_type][variable_name]
     end
 
     def set_value_for(scope_type, variable_name, value)
-      @registry[scope_type][variable_name] = value
+      registry[scope_type][variable_name] = value
+    end
+
+    private
+
+    def registry
+      Thread.current[to_s] ||= Hash.new { |hash, key| hash[key] = {} }
     end
   end
 end
